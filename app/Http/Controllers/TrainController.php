f7498dd0ad68469b501e257f1105fee4cc046621
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-24 19:37:07 +0200

    FIX: fixed the TrainController file, to get the trains that leave after the current date

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Train;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class TrainController extends Controller
 {
@@ -11,7 +12,8 @@ class TrainController extends Controller
     public function index()
     {
 
-        $trains = Train::all();
+        $now = Carbon::now();
+        $trains = Train::all()->where('data', '>', $now);
 
 
         return view('home', compact('trains'));
